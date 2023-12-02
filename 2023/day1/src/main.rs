9990dfe43c9e9b4b0b715e6fee984256e6d8f40e
Nathan <morenate2000@gmail.com>
2023-12-01 20:05:24 -0800

    Completed first part of day 1

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,9 +1,43 @@
+use std::fs;
+
 fn main() {
-    println!("Calibration Value: ");
+    let contents = match fs::read_to_string("day1/input.txt") {
+        Ok(s) => s,
+        Err(_) => {
+            println!("Invalid input");
+            return ();
+        }
+    };
+    let calibration = get_calibration_value(&contents);
+    println!("The calibration value is {calibration}")
 }
 
 fn get_calibration_value(input: &str) -> u32 {
-    142
+    let mut value: u32 = 0;
+    for lines in input.lines() {
+        let mut tens_value: Option<u32> = None;
+        let mut ones_value: Option<u32> = None;
+
+        for char in lines.chars() {
+            match String::from(char).parse::<u32>() {
+                Ok(n) => {
+                    if tens_value == None {
+                        tens_value = Some(n);
+                    } else {
+                        ones_value = Some(n);
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        if ones_value == None {
+            ones_value = tens_value;
+        }
+
+        value += (tens_value.unwrap() * 10) + ones_value.unwrap();
+    }
+    value
 }
 
 #[test]
