8c10df30275289ae948c01392c14aaef3628fb51
Nathan <morenate2000@gmail.com>
2023-12-01 18:59:50 -0800

    Setup day 1 test

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Calibration Value: ");
+}
+
+fn get_calibration_value(input: &str) -> u32 {
+    142
+}
+
+#[test]
+fn example_input()
+{
+    let calibration = get_calibration_value(
+        "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet"
+    );
+    assert_eq!(142, calibration);
 }
